d7854fc2cec8162534a63258d6a69a0454852cbb
michiL96 <michela.lorandi@gmail.com>
2020-09-03 19:02:18 +0200

    Business logic search updated

--- business_logic/views.py
+++ business_logic/views.py
@@ -14,16 +14,17 @@ import json
 class SearchBusinessView(View):
     def get(self, request):
         parameters = request.GET
+        # print(parameters.get('shop_enum', None))
 
-        response_query = requests.get(f"http://{settings.SERVICE_QUERY_SELECTION_HOST}:{settings.SERVICE_QUERY_SELECTION_PORT}/{settings.SERVICE_QUERY_SELECTION}/query_selection", parameters)
+        response_query = requests.get(f"http://{settings.SERVICE_UTILITY_HOST}:{settings.SERVICE_UTILITY_PORT}/{settings.SERVICE_UTILITY}/query_selection", parameters)
         query = json.loads(response_query.content)['query']
-        returned_params = json.loads(response_query.content)['returned_params']
+        # returned_params = json.loads(response_query.content)['returned_params']
 
         json_parameters = dict(parameters)
         json_parameters['query'] = query
 
         # print(json_parameters)
-
+        # print(f"http://{settings.SERVICE_KNOWLEDGE_HOST}:{settings.SERVICE_KNOWLEDGE_PORT}/{settings.SERVICE_KNOWLEDGE}/queries")
         response = requests.get(f"http://{settings.SERVICE_KNOWLEDGE_HOST}:{settings.SERVICE_KNOWLEDGE_PORT}/{settings.SERVICE_KNOWLEDGE}/queries", json_parameters)
         # print(response.content)
         json_response = json.loads(response.content)
@@ -32,7 +33,7 @@ class SearchBusinessView(View):
                      "fulfillmentMessages": [
                          {
                            "text": {
-                             "text": response_templates(query, json_response)
+                             "text": response_templates(query, json_response['results'], parameters)
                            }
                          }
                        ]
@@ -47,11 +48,52 @@ class SearchBusinessView(View):
         return JsonResponse(response)
 
 
-def response_templates(query, results):
+def response_templates(query, results, parameters):
     messages = []
+    print(results)
 
     if query == '3':
-        for res in results['results']:
-            messages.append('The hotel ' + res['name'] + ' starts checkin at ' + res['starthour'] + ' and ends at ' + res['endhour'] + '.')
+        template = 'The hotel {name} starts checkin at {starthour} and ends at {endhour}.'
+        for res in results:
+            messages.append(template.format(name=res['name'], starthour=res['starthour'], endhour=res['endhour']))
+    elif query == '6':
+        template = 'The hotel {name} in {city} starts checkin at {starthour} and ends at {endhour}.'
+        for res in results:
+            messages.append(template.format(name=res['name'], city=res['city'], starthour=res['starthour'], endhour=res['endhour']))
+    elif query == '4':
+        template = 'The {shop_enum} in {region} are: {shops}.'
+        tmp = []
+        for res in results:
+            tmp.append(res['name'] + ' ('+res['city']+')')
+        messages.append(template.format(shop_enum=normalize_enum(parameters.get('shop_enum', None)), region=parameters.get('region', None), shops=', '.join(tmp)))
+    elif query == '5':
+        template = 'The {shop_enum} in {city} ({region}) are: {shops}.'
+        tmp = []
+        for res in results:
+            tmp.append(res['name'])
+        messages.append(template.format(shop_enum=normalize_enum(parameters.get('shop_enum', None)), city=parameters.get('city', None), region=parameters.get('region', None), shops=', '.join(tmp)))
+    elif query == '7':
+        template = 'The {shop_enum} {name} is in {address}, {city} ({region}).'
+        for res in results:
+            messages.append(template.format(shop_enum=normalize_enum(parameters.get('shop_enum', None)), name=res['name'], address=res['street'], city=res['city'], region=normalize_from_ontology(res['province'])))
+    elif query == '8':
+        template = 'The {difficulty} difficulty activity paths with duration {time} are: {paths}.'
+    elif query == '9':
+        template = 'The {difficulty} difficulty activity paths {equipment} are: {paths}.'
+        path_template = 'activity path {name} starts in {poi_from} and ends in {poi_to}'
+        tmp = []
+        for res in results:
+            tmp.append(path_template.format(name=res['name'], poi_from=res['poi_from'], poi_to=res['poi_to']))
+        messages.append(template.format(difficulty=parameters.get('path_difficulty', None), equipment=parameters.get('info_equipment', None), paths='; '.join(tmp)))
 
     return messages
+
+
+def normalize_from_ontology(value):
+    return value.replace('http://www.semanticweb.org/aleca/ontologies/2019/10/untitled-ontology-10#', '')
+
+
+def normalize_enum(value):
+    if value == 'S_Bikes':
+        return 'bike shop'
+    return value
